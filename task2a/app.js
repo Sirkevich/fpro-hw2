ff11e2e5ecc06cb5eb503eb6291b9754a6af577c
Ihor SS <62531809+Sirkevich@users.noreply.github.com>
2020-11-15 21:14:38 +0200

    do task2a

---
+++ task2a/app.js
@@ -0,0 +1,7 @@
+let firstUserNumber = +prompt('Enter your first number');
+let secondUserNumber = +prompt('Enter your second number');
+let thirdUserNumber = +prompt('Enter your third number');
+
+let result = (firstUserNumber + secondUserNumber + thirdUserNumber) / 3;
+
+console.log(+result);
