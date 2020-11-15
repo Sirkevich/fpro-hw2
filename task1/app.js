1efa2f416668882d6cd27bc3060aa99bb2ccbe30
Ihor SS <62531809+Sirkevich@users.noreply.github.com>
2020-11-15 21:10:40 +0200

    do task1

---
+++ task1/app.js
@@ -0,0 +1,8 @@
+const randomNumber = +Math.floor(Math.random() * (100 - 1) + 1);
+
+console.log('Value is ', randomNumber);
+console.count('Value is  ' + randomNumber);
+console.dirxml('Value is ', randomNumber);
+console.info('Value is ', randomNumber);
+console.warn('Value is ', randomNumber);
+console.error('Value is ', randomNumber);
